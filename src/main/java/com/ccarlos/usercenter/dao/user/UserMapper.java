f064b2d62322de71b4e326f5fb9c45411d2f5899
ccarlosf <2238513100@qq.com>
2020-04-16 12:31:32 +0800

    3-14 解决IDEA的红色警告

--- src/main/java/com/ccarlos/usercenter/dao/user/UserMapper.java
+++ src/main/java/com/ccarlos/usercenter/dao/user/UserMapper.java
@@ -3,5 +3,11 @@ package com.ccarlos.usercenter.dao.user;
 import com.ccarlos.usercenter.domain.entity.user.User;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * 作业2：研究@Repository @Component @Service @Controller之间的区别与联系
+ * // 面试题，加分 6
+ */
+//@Repository
+//@Component
 public interface UserMapper extends Mapper<User> {
 }
