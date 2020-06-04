57144bd7208a8e245030b7853001319082b9f79b
ccarlosf <2238513100@qq.com>
2020-06-04 18:06:36 +0800

    9-16 Spring Cloud Stream-编写消费者

--- src/main/java/com/ccarlos/usercenter/UserCenterApplication.java
+++ src/main/java/com/ccarlos/usercenter/UserCenterApplication.java
@@ -2,11 +2,15 @@ package com.ccarlos.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.messaging.Sink;
 import tk.mybatis.spring.annotation.MapperScan;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
 /** 用户中心启动类 */
 // 扫描mybatis哪些包里面的接口
 @MapperScan("com.ccarlos.usercenter.dao")
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class UserCenterApplication {
 
     public static void main(String[] args) {
