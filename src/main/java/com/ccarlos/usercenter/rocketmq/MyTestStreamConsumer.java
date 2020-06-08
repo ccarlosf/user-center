7301b7578c529b2c4327064e89bb41a8956058d0
ccarlosf <2238513100@qq.com>
2020-06-08 17:04:37 +0800

    9-22 Spring Cloud Stream异常处理

--- src/main/java/com/ccarlos/usercenter/rocketmq/MyTestStreamConsumer.java
+++ src/main/java/com/ccarlos/usercenter/rocketmq/MyTestStreamConsumer.java
@@ -3,6 +3,8 @@ package com.ccarlos.usercenter.rocketmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +12,14 @@ import org.springframework.stereotype.Service;
 public class MyTestStreamConsumer {
 
     @StreamListener(MySink.MY_INPUT)
-    public void receive(String messageBody){
-        log.info("自定义接口消息：通过stream收到了消息：messageBody = {}",messageBody);
+    public void receive(String messageBody) {
+        log.info("自定义接口消息：通过stream收到了消息：messageBody = {}", messageBody);
+        throw new IllegalArgumentException("抛异常");
+    }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message) {
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        log.warn("发生异常，errorMessage：{}", errorMessage);
     }
 }
