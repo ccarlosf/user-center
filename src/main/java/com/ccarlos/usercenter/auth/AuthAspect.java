b29fb0cafe66c33936e5874346c07cd236b6afba
ccarlosf <2238513100@qq.com>
2020-06-23 20:59:25 +0800

    11-16 AOP实现用户权限验证

--- src/main/java/com/ccarlos/usercenter/auth/AuthAspect.java
+++ src/main/java/com/ccarlos/usercenter/auth/AuthAspect.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
@@ -13,6 +14,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -36,7 +39,7 @@ public class AuthAspect {
             // 2. 校验token是否合法&是否过期；如果不合法或已过期直接抛异常；如果合法放行
             Boolean isValid = jwtOperator.validateToken(token);
             if (!isValid) {
-                throw  new SecurityException("Token不合法！");
+                throw new SecurityException("Token不合法！");
             }
 
             // 3. 如果校验成功，那么就将用户的信息设置到request的attribute里面
@@ -51,6 +54,7 @@ public class AuthAspect {
 
     /**
      * 静态方法获取request对象
+     *
      * @return
      */
     private HttpServletRequest getHttpServletRequest() {
@@ -59,4 +63,34 @@ public class AuthAspect {
         return attributes.getRequest();
     }
 
+    /**
+     * 校验后台管理员审核权限
+     *
+     * @param point
+     * @return
+     * @throws Throwable
+     */
+    @Around("@annotation(com.ccarlos.usercenter.auth.CheckAuthorization)")
+    public Object checkAuthorization(ProceedingJoinPoint point) throws Throwable {
+        try {
+            // 1. 验证token是否合法；
+            this.checkToken();
+            // 2. 验证用户角色是否匹配
+            HttpServletRequest request = getHttpServletRequest();
+            String role = (String) request.getAttribute("role");
+
+            MethodSignature signature = (MethodSignature) point.getSignature();
+            Method method = signature.getMethod();
+            CheckAuthorization annotation = method.getAnnotation(CheckAuthorization.class);
+
+            String value = annotation.value();
+
+            if (!Objects.equals(role, value)) {
+                throw new SecurityException("用户无权访问！");
+            }
+        } catch (Throwable throwable) {
+            throw new SecurityException("用户无权访问！", throwable);
+        }
+        return point.proceed();
+    }
 }
